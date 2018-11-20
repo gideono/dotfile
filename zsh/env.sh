c04ec8014ac635fe5b6bde75d0ba186dd6851755
Gideon Oduro <gideonoduro@outlook.com>
2018-11-20 09:14:50 +0100

    update env

--- zsh/env.sh
+++ zsh/env.sh
@@ -14,12 +14,10 @@ function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
 # whatsRunning 8080 | awk '{ print $2; }' maybe new funfaction that pips to clipboard
 function whatsRunning (){ lsof -n -i :$1 | grep LISTEN }
+function whatsRunningc (){ lsof -n -i :$1 | grep LISTEN | awk '{ print $2; }' | pbcopy }
 function mkcd() { mkdir -p "$@" && cd "$_"; }
 function pwdc() { pwd|pbcopy }
-function brew-install(){
- local package=$1
- shift && brew install "$package" && brew-install "$@"
-}
+
 
 # ssh dev.tdcserviceonline.com -R 45569:localhost:4200 -l segidodu
 
